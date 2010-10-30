f9ff98070bae8cdbc1c990d916a30150c9abb24b
TOKI Yoshinori <toki@freedom.ne.jp>
2010-10-30 18:39:24 +0900

    simple example.

---
+++ example.rb
@@ -0,0 +1,16 @@
+#!/usr/local/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'ezup'
+
+def ezup_main(env)
+  [ 200, { 'Content-Type' => 'text/html' },
+    "<html><body><p>Hello world.</p></body></html>" ]
+end
+
+ezup_run if $0 == __FILE__
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
